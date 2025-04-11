a183c685ac006a985a0fc548a189ce6e80b69ce8
Rizey777 <rifkirizey@gmail.com>
2025-04-11 09:57:00 +0700

    initial codes

---
+++ laravel-calculator/app/Models/Calculator.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Calculator extends Model
+{
+    public static function calculate($num1, $num2, $operation)
+    {
+        switch ($operation) {
+            case 'add':
+                return $num1 + $num2;
+            case 'subtract':
+                return $num1 - $num2;
+            case 'multiply':
+                return $num1 * $num2;
+            case 'divide':
+                if ($num2 == 0) {
+                    return "Error: Division by zero!";
+                }
+                return $num1 / $num2;
+            default:
+                return "Error: Invalid operation!";
+        }
+    }
+}
